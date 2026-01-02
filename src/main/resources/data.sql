83524b847317f33eb17f1bf603882f7993f9dd7e
avidee007 <avidee0326@gmail.com>
2026-01-02 20:30:07 +0530

    Added init script to load leader transaction data at server startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT
+INTO "ledger_transaction" ("txn_id", "amount")
+VALUES ('GTX1', 500.00),
+       ('GTX2', 1600.00),
+       ('GTX3', 10.00),
+       ('GTX4', 170.00),
+       ('GTX5', 1800.00),
+       ('GTX9', 90.00);
